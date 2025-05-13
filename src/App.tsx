99141be3cdd4f94042e77bf4eeca351783914782
Sara Costa Cabral Mululo <saramululo@gmail.com>
2025-05-13 16:34:22 -0500

    added supabase listener auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
-
-import './App.css'
+import { useEffect } from 'react';
+import Auth from './components/Auth';
+import { useChatStore } from './store/chatStore';
+import { supabase } from './supabaseClient';
 
 function App() {
-  
-  return (
-    <>
-      
-    </>
-  )
+  const { user, setUser } = useChatStore();
+
+  useEffect(()=>{
+    supabase.auth.getSession().then((response)=> {
+      const session = response.data.session
+      setUser({id: session?.user.id || "", email: session?.user.email ||""});
+    });
+    const {data} = supabase.auth.onAuthStateChange((_event, session)=> {
+      if(session?.user){
+        setUser({id: session?.user.id || "", email: session?.user.email||""});
+      } else {
+        setUser(null);
+      }
+    })
+    const subscription = data.subscription;
+    return ()=> subscription.unsubscribe();
+  }, [])
+
+  if(!user) {
+  return <Auth/>;
+  }
+
+  return <div> Chat App</div>
 }
 
 export default App
--- src/store/chatStore.ts
+++ src/store/chatStore.ts
@@ -4,13 +4,12 @@ interface User {
   id: string;
   email: string;
 }
-
 interface ChatStore {
   user: User| null;
-  setUser: (user:User)=> void;
+  setUser: (user:User|null)=> void;
 }
 
 export const useChatStore = create <ChatStore> ((set)=> ({
   user: null,
-  setUser:(user:User) => set({user:user}),
+  setUser:(user:User|null) => set({user:user}),
 }))
