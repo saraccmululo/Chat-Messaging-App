bccffa8d6cb9a6ac3049981aa41c0243c1c85572
Sara Costa Cabral Mululo <saramululo@gmail.com>
2025-05-15 13:36:50 -0500

    Added currentRoom state to zustand state store

--- src/store/chatStore.ts
+++ src/store/chatStore.ts
@@ -4,12 +4,23 @@ interface User {
   id: string;
   email: string;
 }
+
+interface Room {
+  id: number;
+  name: string;
+}
 interface ChatStore {
   user: User| null;
   setUser: (user:User|null)=> void;
+
+  currentRoom: Room | null;
+  setCurrentRoom: (room:Room|null) => void;
 }
 
 export const useChatStore = create <ChatStore> ((set)=> ({
   user: null,
   setUser:(user:User|null) => set({user:user}),
+
+  currentRoom: {id:1, name: "General"},
+  setCurrentRoom: (room:Room|null) => set({currentRoom:room})
 }))
