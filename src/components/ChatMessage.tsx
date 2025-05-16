7c1c273c827f6acc4f22cf19bd5cbb84e2515684
Sara Costa Cabral Mululo <saramululo@gmail.com>
2025-05-16 14:03:50 -0500

    Added real time functionality with useEffect in ChatMessage component

--- src/components/ChatMessage.tsx
+++ src/components/ChatMessage.tsx
@@ -1,6 +1,7 @@
-import { useQuery } from "@tanstack/react-query";
+import { QueryClient, useQuery, useQueryClient } from "@tanstack/react-query";
 import { useChatStore } from "../store/chatStore";
 import { supabase } from "../supabaseClient";
+import { useEffect } from "react";
 
 interface Message {
   id: number;
@@ -11,55 +12,95 @@ interface Message {
   room_id: number;
 }
 
-const fetchMessages = async (roomId: number): Promise <Message[]> => {
-  const {data, error} = await supabase.from("messages").select("*").eq("room_id", roomId).order("created_at", {ascending:true});
+const fetchMessages = async (roomId: number): Promise<Message[]> => {
+  const { data, error } = await supabase
+    .from("messages")
+    .select("*")
+    .eq("room_id", roomId)
+    .order("created_at", { ascending: true });
 
-  if(error) throw Error(error.message);
-  
+  if (error) throw Error(error.message);
+
   return data as Message[];
-}
+};
 
 const ChatMessage = () => {
-  const {currentRoom, user} = useChatStore();
-
-  const {data:messages, error, isLoading} = useQuery<Message[], Error>({
-    queryKey: ["messages", currentRoom?.id ],
-    queryFn: ()=> {
-      if (!currentRoom || currentRoom.id===null) { 
+  const { currentRoom, user } = useChatStore();
+  const queryClient = useQueryClient();
+  const {
+    data: messages,
+    error,
+    isLoading,
+  } = useQuery<Message[], Error>({
+    queryKey: ["messages", currentRoom?.id],
+    queryFn: () => {
+      if (!currentRoom || currentRoom.id === null) {
         return Promise.resolve([]);
       }
       return fetchMessages(currentRoom.id);
     },
-    enabled: currentRoom?.id!==null,
-  })
+    enabled: currentRoom?.id !== null,
+  });
 
-  if(isLoading) return <p className="loader-text">Loading messages...</p>
-  if(error) return <p className="loader-text">Error loading messages: {error.message}</p>
+  useEffect(() => {
+    if (!currentRoom?.id) return;
+    const channel = supabase.channel("message-channel");
+    channel
+      .on(
+        "postgres_changes",
+        { event: "INSERT", schema: "public", table: "messages" },
+        (payload) => {
+          const newMessage = payload.new as Message;
+          if (newMessage.room_id === currentRoom.id) {
+            queryClient.setQueryData<Message[]>(
+              ["messages", currentRoom?.id],
+              (oldMessages) =>
+                oldMessages ? [...oldMessages, newMessage] : [newMessage]
+            );
+          }
+        }
+      )
+      .subscribe((status) => {
+        console.log("Sub status: ", status);
+      });
 
-  console.log(messages)
+    return () => {
+      supabase.removeChannel(channel);
+    };
+  }, [currentRoom?.id]);
+
+  if (isLoading) return <p className="loader-text">Loading messages...</p>;
+  if (error)
+    return (
+      <p className="loader-text">Error loading messages: {error.message}</p>
+    );
+
+  console.log(messages);
 
   return (
-   <div>
-    {messages?.map((msg: Message, key)=> {
-      const isOwnMessage = msg.user_id === user?.id;
-      const itemClass = isOwnMessage? "right":"left";
-      
-      const date=new Date(msg.created_at)
-      const hour= date.getHours().toString().padStart(2, "0");
-      const minute=date.getMinutes().toString().padStart(2, "0");
-      const formattedTime = `${hour}:${minute}`;
-      
-      return(
-        <div key={key} className={`conv-message-item conv-message-item--${itemClass}`}>
-          <div className="conv-message-value">{msg.content}</div>
-          <div className="conv-message-details">{formattedTime}</div>
-          <div className="conv-message-details">{msg.email}</div>
-        </div>
-      )
-    })}
-   
-   </div>
-  )
-}
+    <div>
+      {messages?.map((msg: Message, key) => {
+        const isOwnMessage = msg.user_id === user?.id;
+        const itemClass = isOwnMessage ? "right" : "left";
 
-export default ChatMessage
+        const date = new Date(msg.created_at);
+        const hour = date.getHours().toString().padStart(2, "0");
+        const minute = date.getMinutes().toString().padStart(2, "0");
+        const formattedTime = `${hour}:${minute}`;
+
+        return (
+          <div
+            key={key}
+            className={`conv-message-item conv-message-item--${itemClass}`}
+          >
+            <div className="conv-message-value">{msg.content}</div>
+            <div className="conv-message-details">{formattedTime}</div>
+            <div className="conv-message-details">{msg.email}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default ChatMessage;
