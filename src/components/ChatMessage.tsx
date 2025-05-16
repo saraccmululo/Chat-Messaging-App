6529da3ce3b6ecfc119af5778158ff7194caecf8
Sara Costa Cabral Mululo <saramululo@gmail.com>
2025-05-16 13:38:31 -0500

    Created UI to display messages on ChatMessage component

--- src/components/ChatMessage.tsx
+++ src/components/ChatMessage.tsx
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { useChatStore } from "../store/chatStore";
+import { supabase } from "../supabaseClient";
 
 interface Message {
   id: number;
@@ -11,26 +12,53 @@ interface Message {
 }
 
 const fetchMessages = async (roomId: number): Promise <Message[]> => {
-  return []
+  const {data, error} = await supabase.from("messages").select("*").eq("room_id", roomId).order("created_at", {ascending:true});
+
+  if(error) throw Error(error.message);
+  
+  return data as Message[];
 }
 
 const ChatMessage = () => {
-  const {currentRoom} = useChatStore();
-  
-  const {} = useQuery<Message[], Error>({
+  const {currentRoom, user} = useChatStore();
+
+  const {data:messages, error, isLoading} = useQuery<Message[], Error>({
     queryKey: ["messages", currentRoom?.id ],
     queryFn: ()=> {
       if (!currentRoom || currentRoom.id===null) { 
         return Promise.resolve([]);
       }
-      return fetchMessages(currentRoom.id)
-    }
+      return fetchMessages(currentRoom.id);
+    },
+    enabled: currentRoom?.id!==null,
   })
 
+  if(isLoading) return <p className="loader-text">Loading messages...</p>
+  if(error) return <p className="loader-text">Error loading messages: {error.message}</p>
+
+  console.log(messages)
+
   return (
-    <div>
-      Chat Message
-    </div>
+   <div>
+    {messages?.map((msg: Message, key)=> {
+      const isOwnMessage = msg.user_id === user?.id;
+      const itemClass = isOwnMessage? "right":"left";
+      
+      const date=new Date(msg.created_at)
+      const hour= date.getHours().toString().padStart(2, "0");
+      const minute=date.getMinutes().toString().padStart(2, "0");
+      const formattedTime = `${hour}:${minute}`;
+      
+      return(
+        <div key={key} className={`conv-message-item conv-message-item--${itemClass}`}>
+          <div className="conv-message-value">{msg.content}</div>
+          <div className="conv-message-details">{formattedTime}</div>
+          <div className="conv-message-details">{msg.email}</div>
+        </div>
+      )
+    })}
+   
+   </div>
   )
 }
 
