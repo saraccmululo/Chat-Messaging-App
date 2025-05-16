31003d7c8924ff712187ba3270b37255034d9f1b
Sara Costa Cabral Mululo <saramululo@gmail.com>
2025-05-16 13:38:05 -0500

    Configured QueryClient on main file

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter as Router } from 'react-router'
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 
+
+const client = new QueryClient();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Router>
+    <QueryClientProvider client={client}>
+      <Router>
        <App />
-    </Router>
+     </Router>
+    </QueryClientProvider>
   </StrictMode>,
 )
